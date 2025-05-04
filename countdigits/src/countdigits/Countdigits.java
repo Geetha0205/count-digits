f1788e27cb519bb609f4551bdbad6aac5e1ba25a
Geetha0205 <156334215+Geetha0205@users.noreply.github.com>
2025-05-04 06:04:35 +0530

    Add files via upload

---
+++ countdigits/src/countdigits/Countdigits.java
@@ -0,0 +1,24 @@
+package countdigits;
+
+import java.util.*;
+
+public class Countdigits {
+
+   
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        
+        System.out.println("Enter a number:");
+        int n=sc.nextInt();
+        
+        int count=0;
+        
+        while(n>0)
+        {
+            n=n/10;
+            count++;
+        }
+       System.out.println("Count of number is:"+count);
+    }
+    
+}
